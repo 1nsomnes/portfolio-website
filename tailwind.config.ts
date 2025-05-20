e257892f61226438f00e1da6e3ad150a198a43a0
1nsomnes <1nsomnes@users.noreply.github.com>
2025-05-20 02:05:23 -0700

    get rid of artifact on right

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ const config: Config = {
     "*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
+    container: {
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '100%',
+        xl: '100%',
+        '2xl': '100%',
+      },
+    },
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
